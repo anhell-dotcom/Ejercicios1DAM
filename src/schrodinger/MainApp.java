9221be8b0f221e945d2095197c3cdd1a05f4c2e3
anhell-dotcom <dakkon@gmx.es>
2021-03-13 12:01:17 +0100

    primer commit

---
+++ src/schrodinger/MainApp.java
@@ -0,0 +1,21 @@
+package schrodinger;
+
+public class MainApp {
+
+	/**
+	 * Cread la clase genérica CajaSchrodinger que no solo aceptará gatos. Aceptará
+	 * cualquier tipo de animal pero solo animales.
+	 * Después, cread una lista de animales (5) y recorredla. Iréis introduciendo
+	 * uno por uno en la caja. La caja tendrá un método que se llamará observar.
+	 * Aleatoriamente la mitad de las veces que se observe el animal morirá, por lo
+	 * que el método observar devolverá NULL y en la lista se de animales dejará de
+	 * existir ese animal y se guardará NULL pues ha muerto. Si sobrevive, se le
+	 * dejará en la lista.
+	 * Por último mostrar todos los animales que han sobrevivido a la caja de
+	 * Schrödinger.
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/schrodinger/Tortugas.java
@@ -0,0 +1,5 @@
+package schrodinger;
+
+public class Tortugas {
+
+}
